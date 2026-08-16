031e6a066375566e996f51cfeb373f68c8012376
Xuan Yang <xuanyang.robotics@gmail.com>
2026-08-16 10:28:46 +0800

    feat(v3-01): scaffold runtime contract validator package

---
+++ tools/runtime_contracts/__init__.py
@@ -0,0 +1,8 @@
+from .validator import ValidationIssue, ValidationReport, load_yaml, validate_vehicle_profile
+
+__all__ = [
+    "ValidationIssue",
+    "ValidationReport",
+    "load_yaml",
+    "validate_vehicle_profile",
+]
