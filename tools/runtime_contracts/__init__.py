23f87a2e2bd3649d6168ed5dd6e92ac82f338240
Xuan Yang <xuanyang.robotics@gmail.com>
2026-08-16 10:35:01 +0800

    refactor(v3-01): export runtime contract validators

--- tools/runtime_contracts/__init__.py
+++ tools/runtime_contracts/__init__.py
@@ -1,8 +1,17 @@
-from .validator import ValidationIssue, ValidationReport, load_yaml, validate_vehicle_profile
+from .validator import (
+    ValidationIssue,
+    ValidationReport,
+    load_yaml,
+    validate_runtime_contracts,
+    validate_site_package,
+    validate_vehicle_profile,
+)
 
 __all__ = [
     "ValidationIssue",
     "ValidationReport",
     "load_yaml",
+    "validate_runtime_contracts",
+    "validate_site_package",
     "validate_vehicle_profile",
 ]
