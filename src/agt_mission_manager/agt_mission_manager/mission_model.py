4fa7849696755b7529a76c7a3cd959656aa0d334
Xuan Yang <xuanyang.robotics@gmail.com>
2026-08-27 23:20:27 +0800

    feat(mission): add inspection task step model

--- src/agt_mission_manager/agt_mission_manager/mission_model.py
+++ src/agt_mission_manager/agt_mission_manager/mission_model.py
@@ -20,6 +20,7 @@ class StepType(IntEnum):
     WAYPOINT_TASK = 1
     WAIT_DURATION = 2
     WAIT_EVENT = 3
+    INSPECTION_TASK = 4
 
 
 class MissionState(IntEnum):
@@ -69,6 +70,9 @@ class MissionStep:
     event_source: str = ""
     correlation_id: str = ""
     timeout_s: float = 0.0
+    inspection_task_id: str = ""
+    inspection_task_revision: int = 0
+    expected_content_sha256: str = ""
 
 
 @dataclass(frozen=True)
