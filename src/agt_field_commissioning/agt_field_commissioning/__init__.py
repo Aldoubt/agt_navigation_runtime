aea17873fc5841af2d8287d25f28e3dd940cd443
Xuan Yang <xuanyang.robotics@gmail.com>
2026-08-29 15:17:39 +0800

    feat(commissioning): add mapping artifact finalizer

---
+++ src/agt_field_commissioning/agt_field_commissioning/__init__.py
@@ -0,0 +1,9 @@
+"""Field commissioning helpers for deterministic map capture runs."""
+
+from .mapping_artifacts import CommissioningRunPaths, finalize_mapping_run, prepare_mapping_run
+
+__all__ = [
+    "CommissioningRunPaths",
+    "finalize_mapping_run",
+    "prepare_mapping_run",
+]
